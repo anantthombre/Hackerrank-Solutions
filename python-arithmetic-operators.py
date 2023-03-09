c2a679fb19bd1051020a7238383c69249acce28d
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-03-09 17:48:49 +0530

    Create python-arithmetic-operators.py

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,10 @@
+# Link to the coding question -  https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    

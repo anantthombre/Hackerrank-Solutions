b0722685e74fd46f807500784ed1abcf56dc2e1b
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-03-09 17:56:55 +0530

    created python-loops.py

---
+++ python-loops.py
@@ -0,0 +1,8 @@
+Link to the coding question -  https://www.hackerrank.com/challenges/python-loops
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i*i)
+        

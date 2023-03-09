3aa0ef4a6063e28f8a6d4684d0f986e5634568c7
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-03-09 19:19:23 +0530

    created python-print.py

---
+++ python-print.py
@@ -0,0 +1,8 @@
+# Link to the coding question -  https://www.hackerrank.com/challenges/python-print
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(1,n+1):
+        print(i, end="")
+        

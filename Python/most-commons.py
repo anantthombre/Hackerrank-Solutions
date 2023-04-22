2b56df4d74d8ab26f5d35788399ca37cc2a944eb
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-04-22 15:26:22 +0530

    Create most-commons.py

---
+++ Python/most-commons.py
@@ -0,0 +1,20 @@
+#Link to the coding question -  https://www.hackerrank.com/challenges/most-commons
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+
+if __name__ == '__main__':
+    s = input()
+    s = sorted(s)
+    
+    Frequency = Counter(list(s))
+    for k,v in Frequency.most_common(3):
+        print(k, v)
+ 

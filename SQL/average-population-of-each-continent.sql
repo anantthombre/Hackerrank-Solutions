5a9a649ffce72f5ccf8fa07fb238a479ad82403b
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-04-25 13:32:42 +0530

    Update average-population-of-each-continent.sql

--- SQL/average-population-of-each-continent.sql
+++ SQL/average-population-of-each-continent.sql
@@ -1,1 +1,7 @@
 # Link to the coding question: https://www.hackerrank.com/challenges/average-population-of-each-continent/
+
+
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
+FROM CITY
+INNER JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;

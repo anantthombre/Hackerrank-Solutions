aa56a48e64c2bbc6bdacff3c6444fb7f2226537c
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-04-26 20:05:29 +0530

    Create the-report.sql

---
+++ SQL/the-report.sql
@@ -0,0 +1,12 @@
+# Link to the coding question -  https://www.hackerrank.com/challenges/the-report
+
+/*
+Enter your query here.
+*/
+
+SELECT 
+CASE WHEN grd.grade < 8 THEN NULL 
+WHEN grd.grade >= 8 THEN std.name END,
+grd.grade, std.marks FROM students std, grades grd
+WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
+ORDER BY grd.grade DESC, std.name ASC;

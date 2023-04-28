8009b14e8407973d3b096725d39f548790f5ec02
Anant Thombre <70723143+anantthombre@users.noreply.github.com>
2023-04-28 23:15:24 +0530

    Create full-score.sql

---
+++ SQL/full-score.sql
@@ -0,0 +1,14 @@
+# Link to the coding question - https://www.hackerrank.com/challenges/full-score
+
+/*
+Enter your query here.
+*/
+SELECT S.hacker_id, name
+FROM SUBMISSIONS AS S
+JOIN HACKERS AS H ON S.hacker_id = H.hacker_id
+JOIN Challenges AS C ON S.challenge_id = C.challenge_id
+JOIN Difficulty AS D ON C.difficulty_level = D.difficulty_level
+WHERE S.score = D.score
+GROUP BY name, S.hacker_id
+HAVING count(S.challenge_id) > 1
+ORDER BY count(S.challenge_id) DESC, S.hacker_id
